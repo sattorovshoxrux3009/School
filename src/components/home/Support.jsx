2070c70fb90d83d4664c2382f743e4eb3e9e178c
sattorovshoxrux3009 <sattorovshoxrux3009@gmail.com>
2025-04-25 13:32:01 +0500

    add responsivity Support.jsx

--- src/components/home/Support.jsx
+++ src/components/home/Support.jsx
@@ -1,33 +1,52 @@
 import "./Support.scss";
+import { easeOut, motion } from "framer-motion";
+import img1 from "../../img/teacher1.png";
+import img2 from "../../img/teacher2.png";
+import img3 from "../../img/teacher3.png";
+import img4 from "../../img/teacher4.png";
+import { useTranslation } from "react-i18next";
 
 function Support() {
+  const { t } = useTranslation();
+
   return (
-    <div className="support">
+    <section className="support">
       <div className="container">
-        <div className="left">
+        <motion.div
+          whileInView={{ y: [100, 1] }}
+          transition={{ duration: 0.3, ease: easeOut }}
+          className="left"
+        >
           <ul>
             <li>
-              <h1>200+</h1>
-              <p>maktabdagi o'quvchilar</p>
+              <div>
+                <h1>200+</h1>
+                <p>{t("pupils")}</p>
+              </div>
+              <div>
+                <h1>3</h1>
+                <p>{t("food")}</p>
+              </div>
             </li>
             <li>
-              <h1>3</h1>
-              <p>mahal ovqat</p>
-            </li>
-            <li>
-              <h3>
-                Woonder Kids shunchaki o'qish joyi emas, balki har bir bolaga
-                shaxsiy yordam va e'tibor qaratiladigan joy.
-              </h3>
+              <h3>{t("helpPlace")}</h3>
             </li>
           </ul>
-        </div>
-        <div className="right">
-          <h1>Professional o'qituvchilar</h1>
-          <p>
-            30+ o'qituvchi – bu bizning o'quvchilarimiz muvaffaqiyatining
-            kaliti.
-          </p>
+        </motion.div>
+        <motion.a
+          href=""
+          whileInView={{ y: [100, 1] }}
+          transition={{ duration: 0.3, ease: easeOut }}
+          className="right"
+        >
+          <h1>{t("professionalTeachers")}</h1>
+          <p>{t("keySuccess")}</p>
+          <div>
+            <img className="img1" src={img1} alt="image" />
+            <img className="img2" src={img2} alt="image" />
+            <img className="img3" src={img3} alt="image" />
+            <img className="img4" src={img4} alt="image" />
+          </div>
           <svg
             width="32"
             height="32"
@@ -54,9 +73,9 @@ function Support() {
               stroke-linecap="square"
             ></path>
           </svg>
-        </div>
+        </motion.a>
       </div>
-    </div>
+    </section>
   );
 }
 
