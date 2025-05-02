136672d488f6512624b44f0d105e593933fd781f
sattorovshoxrux3009 <sattorovshoxrux3009@gmail.com>
2025-05-03 00:04:54 +0500

    Create Choice papge

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,18 @@ import Content from "../components/home/Content.jsx";
 import Methods from "../components/home/Methods.jsx";
 import Events from "../components/home/Events.jsx";
 import ContactForm from "../components/home/ContactForm.jsx";
+import Choice from "../components/home/Choice.jsx";
 
 function Home() {
   return (
     <div className="home">
       <Main />
       <Support />
-      <Content/>
-      <Methods/>
-      <Events/>
-      <ContactForm/>
+      <Content />
+      <Methods />
+      <Events />
+      <Choice />
+      <ContactForm />
     </div>
   );
 }
